7bbe9150133f32d889008bcdd71dab6b0420a042
nicolasmeyer <nsg.meyer@hotmail.fr>
2024-11-19 11:57:02 +0100

    💄 F03: LOGIN > Add styling & background

--- src/assets/components/pages/login/LoginPage.jsx
+++ src/assets/components/pages/login/LoginPage.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import LoginForm from "./LoginForm";
 import Logo from "../../reusable-ui/Logo";
 
+
 const LoginPage = () => {
   
 
@@ -14,12 +15,30 @@ const LoginPage = () => {
 };
 
 const LoginPageStyled = styled.div`
-  background-color: red;
+  
   height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  background-image:linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('/images/burger-background.jpg');
+  background-size: cover; 
+  background-position: center;  
+
+  /* ::before {
+    content: "";
+    background: url('/images/burger-background.jpg');
+    background-size: cover; 
+  background-position: center;
+background-blend-mode: darken;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: -1;
+} */
+
 `;
 export default LoginPage;
 
--- src/assets/components/reusable-ui/Logo.jsx
+++ src/assets/components/reusable-ui/Logo.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 
 export default function Logo() {
   return (
-    <div>Logo</div>
+    <LogoStyled>Logo CRAZY BURGER</LogoStyled>
   )
 }
 
