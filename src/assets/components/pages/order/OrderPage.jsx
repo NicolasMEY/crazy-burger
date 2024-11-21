b4c4463a0de9aef3502e4fb9578e9aae472914a3
nicolasmeyer <nsg.meyer@hotmail.fr>
2024-11-21 17:34:57 +0100

    🚧 F04: ORDER > layout > add navbar and main

--- src/assets/components/pages/order/OrderPage.jsx
+++ src/assets/components/pages/order/OrderPage.jsx
@@ -8,10 +8,14 @@ const OrderPage = () => {
     return (
         <OrderPageStyled>
             <div className='container'>
-                <h1>Bonjour {username}</h1>
-                <Link to="/"><button
-                >Déconnexion</button>
-                </Link>
+                <div className='navbar'>
+                    Navbar
+                    <h1>Bonjour {username}</h1>
+                    <Link to="/"><button
+                    >Déconnexion</button>
+                    </Link>
+                </div>
+                <div className="main">Main</div>
             </div>
         </OrderPageStyled>
     );
@@ -31,6 +35,19 @@ const OrderPageStyled = styled.div`
     background-color: red;
     height: 95vh;
     width: 1400px ;
+    display: flex;
+    flex-direction: column;
+
+    .navbar {
+    background-color: blueviolet;
+    height: 10vh;
+    
+}
+    .main {
+    background-color: green;
+    /* height: calc(95vh - 10VH); // on peut utiliser cette méthode sans display flex au niveau du container */
+    flex: 1;
+}
 }
 
 
