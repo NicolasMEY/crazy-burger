7ddab8c16d96a3f04d38a7631fe65c3ad2bc9bb6
nicolasmeyer <nsg.meyer@hotmail.fr>
2024-11-23 14:18:10 +0100

    💄 F05: ORDER > Product > Add image layout

--- src/assets/components/pages/order/Main/Product.jsx
+++ src/assets/components/pages/order/Main/Product.jsx
@@ -5,11 +5,11 @@ export default function Product({title, imageSource, price}) {
     <ProductStyled className="produit">
           <div className="image"><img src={imageSource} alt={title} /></div>
           <div className="info-text">
-            <div className="description">
             <div className="title">{title}</div>
+            <div className="description">
               <div className="price">{price}</div>
-              </div>
             <button className="add-button">Ajouter</button>
+              </div>
           </div>
 
         </ProductStyled>
@@ -19,21 +19,27 @@ export default function Product({title, imageSource, price}) {
 const ProductStyled = styled.div`
 
   background-color: red;
-  width: 240px;
-  height: 330px;
- 
+  width: 200px;
+  height: 300px;
+  display: grid;
+  grid-template-rows:65% 1fr ;
+  padding: 20px;
+  padding-bottom: 10px;
+
   .image{
-    width: 100px;
+    width: 100%;
     height: auto ;
+    margin-top: 30px;
     img {
       width: 100%;
       height: 100%;
-      object-fit: cover;
+      object-fit: contain;
     }
   }
 
-  .description {
-    border: 1px solid fuchsia;
+  .info-text {
+    border: 1px solid blue;
+    
   }
 
 `;
