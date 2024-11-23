da616a11a0fa331ca6aedfa18074e39acf65903c
nicolasmeyer <nsg.meyer@hotmail.fr>
2024-11-23 19:10:16 +0100

    💄 F06: NAVBAR > Right side > Add spacing

--- src/assets/components/pages/order/Navbar/NavbarRightSide.jsx
+++ src/assets/components/pages/order/Navbar/NavbarRightSide.jsx
@@ -1,29 +1,49 @@
 
 import styled from "styled-components";
 import Profile from "./Profile";
+import { ToastContainer } from "react-toastify";
+import { theme } from "../../../../theme";
+import ToggleButton from "../../../reusable-ui/ToggleButton";
+
 
 function NavbarRightSide({username}) {
     return (
         <NavbarRightSideStyled className="right-side">
-        {/* <div className="admin-button">Admin button</div> */}
-        <Profile username={username}/>
+     <ToggleButton labelIfUnchecked="ACTIVER LE MODE ADMIN" labelIfChecked="DÉSACTIVER LE MODE ADMIN"/>
+    
+        {/* <ToastContainer className="toaster" bodyClassName="body-toast" /> */}
+        <Profile username={username} />
       </NavbarRightSideStyled>
     );
 }
 
 const NavbarRightSideStyled = styled.div`
-
     display: flex;
     align-items: center;
     padding-right: 50px;
+
    
-   /* .admin-button {
-    background-color: lightblue ;
-   } */
+     .toaster {
+    max-width: 300px;
+  }
+
+  .Toastify__toast.Toastify__toast-theme--dark.Toastify__toast--info {
+    background: ${theme.colors.background_dark};
+  }
+
+  .body-toast {
+    .Toastify__toast-icon.Toastify--animate-icon.Toastify__zoom-enter {
+      margin-right: 20px;
+      margin-left: 5px;
+    }
+    div {
+      line-height: 1.3em;
+    }
+  } 
+   
+   
 
     
 `;
-
-
 
 export default NavbarRightSide;
