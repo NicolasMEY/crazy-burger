120ed5fc30790bcb54d11364e064c2430f72ddc7
nicolasmeyer <nsg.meyer@hotmail.fr>
2024-11-26 19:48:13 +0100

    💄 F07: ADMIN > Tabs > Add Tab

--- src/assets/components/pages/order/Main/Admin/AdminTabs.jsx
+++ src/assets/components/pages/order/Main/Admin/AdminTabs.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import styled from 'styled-components';
 import { theme } from '../../../../../theme';
+import Tab from '../../../../reusable-ui/Tab';
+import {FiChevronDown, FiChevronUp} from "react-icons/fi"
 
 export default function AdminTabs() {
   return (
-    <AdminTabsStyled>AdminTabs</AdminTabsStyled>
+    <AdminTabsStyled>
+        <Tab Icon={<FiChevronDown/>}/>
+    </AdminTabsStyled>
   )
 }
 
 const AdminTabsStyled = styled.div`
+display: flex;
+padding: 0 20px;
+
   
-`;
+`
