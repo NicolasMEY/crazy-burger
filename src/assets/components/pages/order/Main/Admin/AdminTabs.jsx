95c721d33943cde472e4b73dece6e07fb99da997
nicolasmeyer <nsg.meyer@hotmail.fr>
2024-11-26 19:02:02 +0100

    💄 F07: ADMIN > Add AdminTabs and AdminPanel

---
+++ src/assets/components/pages/order/Main/Admin/AdminTabs.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components';
+import { theme } from '../../../../../theme';
+
+export default function AdminTabs() {
+  return (
+    <AdminTabsStyled>AdminTabs</AdminTabsStyled>
+  )
+}
+
+const AdminTabsStyled = styled.div`
+  
+`;
