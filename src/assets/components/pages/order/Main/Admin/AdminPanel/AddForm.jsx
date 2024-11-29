08d7324c7466ec200adb121a9bea4a850226862b
nicolasmeyer <nsg.meyer@hotmail.fr>
2024-11-29 12:42:41 +0100

    🚧 F08: ADMIN > AddForm > Add inputs layout

--- src/assets/components/pages/order/Main/Admin/AdminPanel/AddForm.jsx
+++ src/assets/components/pages/order/Main/Admin/AdminPanel/AddForm.jsx
@@ -1,3 +1,47 @@
+import styled from "styled-components";
+
 export default function AddForm() {
-  return <div>AddForm</div>;
+  return <AddFormStyled>
+    <div className="image-preview">ImagePreview</div>
+
+    < div className= "input-fields">
+      <input type="text" placeholder="Nom" />
+    
+      <input type="text" placeholder="Image URL" />
+    
+      <input type="text" placeholder="Prix" />
+    </div> 
+
+    <button className="submit-button">Submit button</button>
+    
+    </AddFormStyled>;
 }
+
+const AddFormStyled = styled.form`
+  border: 5px, solid, black;
+  display: grid;
+  grid-template-columns: 1fr, 3fr;
+  grid-template-rows: repeat(4, 1fr);
+  height: 100%;
+  width: 70%;
+
+  .image-preview {
+    background-color: blue;
+    // Grid-area : coin  supérieur gauche puis coin inférieur droit et on commence toujours par la ligne
+    grid-area: 1 / 1 / 4 / 2 ;
+  }
+  .input-fields {
+    background-color: purple;
+    grid-area: 1 / 2 / 4 / 3 ;
+
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows:repeat(3, 1fr) ;
+  }
+  .submit-button {
+    background-color: green;
+    grid-area: 4 / 2 / 5 / 3 ;
+    width: 50%;
+  }
+
+`;
--- src/assets/components/pages/order/Main/Admin/AdminPanel/AdminPanel.jsx
+++ src/assets/components/pages/order/Main/Admin/AdminPanel/AdminPanel.jsx
@@ -12,16 +12,17 @@ const tabSelected =  getTabSelected  (tabs, currentTabSelected)
 
   return (
     <AdminPanelStyled className='panel-admin'>
-      {currentTabSelected === tabSelected.index && tabSelected.label}
+      {currentTabSelected === tabSelected.index && tabSelected.Content}
       </AdminPanelStyled>
   )
 }
 const AdminPanelStyled = styled.div`
-    height: 251px;
+    height: 250px;
     border-bottom-left-radius: ${theme.borderRadius.extraRound};
     border-bottom-right-radius: ${theme.borderRadius.extraRound};
     background-color: ${theme.colors.white};
     border-top: 1px, solid, ${theme.colors.greyLight};
     box-shadow: ${theme.shadows.subtle};
+    padding: 30px 5%;
 
 `;
