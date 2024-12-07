cf11e4dd51ac6f60452957d3d288507492bf6cb4
nicolasmeyer <nsg.meyer@hotmail.fr>
2024-12-07 19:48:55 +0100

    ♻️  F10: BASKET > Add useMenu.jsx

--- src/assets/components/pages/order/OrderPage.jsx
+++ src/assets/components/pages/order/OrderPage.jsx
@@ -4,48 +4,19 @@ import { theme } from '../../../theme';
 import Navbar from './Navbar/Navbar';
 import Main from './Main/Main';
 import OrderContext from "../../../context/OrderContext.jsx"
-import {fakeMenu } from "../../../fakeData/fakeMenu"
 import { EMPTY_PRODUCT } from '../../../enums/product.jsx';
-import { deepClone } from '../../../../utils/array.jsx';
+import { useMenu } from '../../../hooks/useMenu.jsx';
 
 
 const OrderPage = () => {
     const [isModeAdmin, setIsModeAdmin] = useState(true);
     const [isCollapsed, setIsCollapsed] = useState(true)
-    const [currentTabSelected, setCurrentTabSelected] = useState("edit")
-    const [menu, setMenu] = useState(fakeMenu.MEDIUM)
+    const [currentTabSelected, setCurrentTabSelected] = useState("add")
     const [newProduct, setNewProduct] = useState(EMPTY_PRODUCT)
     const [productSelected, setproductSelected] = useState(EMPTY_PRODUCT)
     const titleEditRef = useRef()
+    const {menu, handleAdd, handleDelete, handleEdit, resetMenu} = useMenu()
 
-
-
-const handleAdd = (newProduct) => {
-            const menuCopy = deepClone(menu);
-            const menuUpdated = [newProduct, ...menuCopy];
-            setMenu(menuUpdated);
-        };
-
-const handleDelete = (idOfProductToDelete) => { 
-        //1. copy du state
-            const menuCopy = deepClone(menu)
-        //2. manip de la copie du state
-            const menuUpdated = menuCopy.filter((product) => product.id !== idOfProductToDelete)
-        //3. update du state
-            setMenu(menuUpdated)
-        }
-
-const handleEdit = (productBeingEdited) =>
-// Copy du state en mode deep clone, alternative plus puissante au destructuring qui reste superficiel [...menu]
-{ const menuCopy = deepClone(menu)
-    //2. Manip de la copie du state
-const indexOfProductToEdit = menu.findIndex((menuProduct) => menuProduct.id === productBeingEdited.id )
-menuCopy [indexOfProductToEdit] = productBeingEdited
-// 3. Update du state
-setMenu(menuCopy)
- }
-
-const resetMenu = () => { setMenu(fakeMenu.MEDIUM) }
 
 const orderContextValue={
     isModeAdmin,
