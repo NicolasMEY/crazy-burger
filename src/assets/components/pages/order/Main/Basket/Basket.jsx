210a4f92a35bde2058097f7ed65688beeaa61946
nicolasmeyer <nsg.meyer@hotmail.fr>
2024-12-07 18:51:42 +0100

    ♻️  F10: BASKET > Add Body

--- src/assets/components/pages/order/Main/Basket/Basket.jsx
+++ src/assets/components/pages/order/Main/Basket/Basket.jsx
@@ -1,15 +1,15 @@
 import styled from "styled-components"
 import { theme } from "../../../../../theme"
-import Header from "../../../../reusable-ui/Header"
 import Total from "./Total"
 import {formatPrice} from"../../../../../../utils/maths"
 import Footer from "./Footer"
+import BasketBody from "./BasketBody"
 
 export default function Basket() {
   return (
     <BasketStyled>
       <Total amountToPay={formatPrice(0)} />
-      <div className="body">body</div>
+      <BasketBody/>
       <Footer />
     </BasketStyled>
   );
@@ -19,9 +19,5 @@ const BasketStyled = styled.div`
   display: flex;
   flex-direction: column;
   
-.body {
-flex: 1;
-background-color: ${theme.colors.background_white};
-box-shadow: ${theme.shadows.basket};
-}
+
 `
