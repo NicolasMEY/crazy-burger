38983291d0974bea5c3041b32410be0a7c771af4
nicolasmeyer <nsg.meyer@hotmail.fr>
2024-12-07 19:15:06 +0100

    ♻️  F10: BASKET > Add useSuccesMessage.jsx

--- src/assets/components/pages/order/Main/Admin/AdminPanel/AddForm.jsx
+++ src/assets/components/pages/order/Main/Admin/AdminPanel/AddForm.jsx
@@ -1,13 +1,14 @@
 import OrderContext from "../../../../../../context/OrderContext";
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { EMPTY_PRODUCT } from "../../../../../../enums/product";
 import Form from "./Form";
 import SubmitButton from "./SubmitButton";
+import { useSuccesMessage } from "../../../../../../hooks/useSuccesMessage.jsx";
 
 
 export default function AddForm() {
 const {handleAdd, newProduct, setNewProduct} = useContext(OrderContext)
-const [isSubmitted,setIsSubmitted] = useState(false)
+const {isSubmitted, displaySuccesMessage} = useSuccesMessage()
 
 
 // comportements
@@ -26,12 +27,6 @@ const handleChange = (event) => {
   const {name, value} = event.target
   setNewProduct({...newProduct, [name]: value }) 
 }
-
-const displaySuccesMessage = () => {setIsSubmitted(true)
-  setTimeout(() => {
-    setIsSubmitted(false)
-  }, 2000);}
-
 
   return (
     <Form product={newProduct} onSubmit={handleSubmit} onChange={handleChange}>
---
+++ src/assets/hooks/useSuccesMessage.jsx.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react"
+
+export const useSuccesMessage = () => {
+    // State
+    const [isSubmitted,setIsSubmitted] = useState(false)
+
+    // Comportement
+    const displaySuccesMessage = () => {
+    setIsSubmitted(true)
+    setTimeout(() => {
+      setIsSubmitted(false)
+    }, 2000)
+}
+
+return {isSubmitted, displaySuccesMessage }
+}
