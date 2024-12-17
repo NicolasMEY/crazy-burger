39e8274681abea6c4eaeee0b2bc1bd6a3a2c4377
nicolasmeyer <nsg.meyer@hotmail.fr>
2024-12-17 22:08:42 +0100

    ✨ F14 : BASKET > Total > Add CasinoEffect

--- src/assets/components/reusable-ui/CasinoEffect.jsx
+++ src/assets/components/reusable-ui/CasinoEffect.jsx
@@ -1,28 +1,48 @@
 import React from 'react'
 import styled from 'styled-components';
+import { TransitionGroup, CSSTransition} from "react-transition-group"
+
 
 export default function CasinoEffect({count,className}) {
   return (
 
-        <TransitionGroup component={CasinoEffectStyled} >
+        <TransitionGroup 
+        component={CasinoEffectStyled} className="transition-group" >
 
-       <CSSTransition className="count-animated" timeout={5000} key={count}
+       <CSSTransition classNames="count-animated" timeout={300} key={count}
         >
         <span className={className}>{count}</span>
         </CSSTransition>
-
         </TransitionGroup>
-    
   )
 }
 const CasinoEffectStyled = styled.div`
+  position: relative;
+  overflow-y: hidden;
+
+span {
+    display: inline-block;
+}
+ /* MOUNTING */
+  .count-animated-enter {
+    transform: translateY(100%) ;
+  }
+  .count-animated-enter-active {
+    transform: translateY(0%);
+    transition: 300ms;
+  }
+  .count-animated-enter-done {}
+
+   /* UNMOUNTING */
+  .count-animated-exit {
+    transform: translateY(0%);
+    position: absolute;
+    right: 0 ;
+    bottom: 0;
+  }
+  .count-animated-exit-active {
+    transform: translateY(-100%);
+    transition: 300ms;
+  }
   
-
-  count-animated-enter {}
-  count-animated-enter-active {}
-  count-animated-enter-done {}
-
-  count-animated-exit {}
-  count-animated-exit-active {}
-  count-animated-exit-done {}
 `;
