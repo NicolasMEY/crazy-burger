a9f5d97e826461c3e88693a95f628613be0674d7
nicolasmeyer <nsg.meyer@hotmail.fr>
2024-12-18 16:58:35 +0100

    ✏️  F14 : MENU > Card > add animation onAdd

--- src/assets/theme/animation.js
+++ src/assets/theme/animation.js
@@ -99,3 +99,26 @@ export const basketAnimation = css`
     opacity: 0%;
   }
 `;
+
+export const menuAnimation = css`
+  /* MOUNTING */
+  .menu-animation-enter {
+    opacity: 0.01;
+    transform: translateX(-120px);
+    &.menu-animation-enter-active {
+      opacity: 1;
+      transform: translateX(0);
+      transition: all ${theme.animation.speed.quick} ease-out;
+    }
+  }
+
+  /* UNMOUNTING */
+  .menu-animation-exit {
+    opacity: 1;
+    transform: translateY(0);
+    &.menu-animation-exit-active {
+      opacity: 0.01;
+      transition: ${theme.animation.speed.quick} ease-out;
+    }
+  }
+`;
