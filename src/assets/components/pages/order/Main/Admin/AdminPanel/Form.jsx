7d25dbb9a6d4d6779744e9ad3b75f964614b07a1
nicolasmeyer <nsg.meyer@hotmail.fr>
2024-12-20 15:14:29 +0100

    🚧 F15: FORM > Add reusable <SelectInput/> (still ugly) in JSX

--- src/assets/components/pages/order/Main/Admin/AdminPanel/Form.jsx
+++ src/assets/components/pages/order/Main/Admin/AdminPanel/Form.jsx
@@ -3,6 +3,16 @@ import styled from "styled-components";
 import TextInput from "../../../../../reusable-ui/TextInput"
 import ImagePreview from "./ImagePreview";
 import { getInputTextsConfig } from "./inputTextConfig";
+import SelectInput from "../../../../../reusable-ui/SelectInput";
+
+const isAvailableOptions = [
+  {value: true, label: "En stock"},
+  {value: false, label: "En rupture"},
+]
+const isPublicisedOptions = [
+  {value: true, label: "Sans pub"},
+  {value: false, label: "Avec pub"},
+]
 
 const Form = React.forwardRef(({product, onSubmit, onChange, onFocus, onBlur, children}, ref) =>  {
 
@@ -23,14 +33,14 @@ const inputTexts = getInputTextsConfig(product)
     onBlur={onBlur}
     ref={ref && input.name === "title" ? ref : null}
     />))}
-    <select name="isAvailable" className="is-available" id="3">
-      <option value={true}>En stock</option>
-      <option value={false}>En rupture</option>
-    </select>
-    <select name="isPublicised" className="is-publicised" id="4">
-      <option value={true}>Sans pub</option>
-      <option value={false}>Avec pub</option>
-    </select>
+    <SelectInput 
+    name={isAvailableOptions} 
+    options={isAvailableOptions} className="is-available" 
+    id="3"/>
+    <SelectInput 
+    name={isPublicisedOptions} 
+    options={isPublicisedOptions} className="is-publicised" 
+    id="4"/>
     </div>
     <div className="form-footer">{children}</div>
     </FormStyled>
---
+++ src/assets/components/reusable-ui/SelectInput.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components';
+
+export default function SelectInput({ options,name, className, id}) {
+  return (
+    <SelectInputStyled name={name} className={className} id={id}>
+      {options.map(({value, label}) => (<option value={value}>{label}</option>))}
+    </SelectInputStyled>
+  )
+}
+
+const SelectInputStyled = styled.select`
+  
+`;
