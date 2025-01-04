cf1414947597459b6b18c5d5836654725e816930
nicolasmeyer <nsg.meyer@hotmail.fr>
2025-01-04 16:42:43 +0100

    ♻️ TSE-04:  add hooks.ts

--- src/assets/hooks/useBasket.ts
+++ src/assets/hooks/useBasket.ts
@@ -1,13 +1,13 @@
 import { useState } from "react"
-import { fakeBasket } from "../fakeData/fakeBasket"
 import {deepClone, findIndexById, findObjectById, removeObjectById} from "../../utils/array"
 import { setLocalStorage } from "../../utils/window"
+import { BasketProduct } from "@/types/Product"
+
 
 export const useBasket = () => {
-  const [basket, setBasket] = useState([])
+  const [basket, setBasket] = useState<BasketProduct[]>([])
 
-
-const handleAddToBasket = (idProductToAdd, username) => { 
+const handleAddToBasket = (idProductToAdd : string, username : string) => { 
 const basketCopy = deepClone(basket)
 const productAlreadyInBasket = findObjectById(idProductToAdd, basketCopy)
 
@@ -19,26 +19,24 @@ if(productAlreadyInBasket){
   createNewBasketProduct(idProductToAdd, basketCopy, setBasket, username)
  }
 
-const incrementProductAlreadyInBasket = (idProductToAdd, basketCopy, username) => {
+const incrementProductAlreadyInBasket = (idProductToAdd : string, basketCopy : BasketProduct[], username : string) => {
   const indexOfBasketProductToIncrement = findIndexById(idProductToAdd, basketCopy)
   basketCopy[indexOfBasketProductToIncrement].quantity += 1
   setBasket(basketCopy)
   setLocalStorage( username,basketCopy )
    }
 
-  const createNewBasketProduct = (idProductToAdd, basketCopy, setBasket, username) => {
+  const createNewBasketProduct = (idProductToAdd : string, basketCopy : BasketProduct[], setBasket : React.Dispatch<React.SetStateAction<BasketProduct[]>>, username : string) => {
     const newBasketProduct = { id: idProductToAdd, quantity: 1 }
     const newbasket = [newBasketProduct, ...basketCopy]
     setBasket(newbasket)
     setLocalStorage( username,newbasket )
-
   }
 
-  const handleDeleteBasketProduct = (idBasketProduct, username) => {
+  const handleDeleteBasketProduct = (idBasketProduct : string, username : string) => {
     const basketUpdated = removeObjectById(idBasketProduct, basket)
     setBasket(basketUpdated)
     setLocalStorage( username,basketUpdated )
-
   }
 
   return { basket, setBasket,  handleAddToBasket, handleDeleteBasketProduct }
